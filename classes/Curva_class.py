bb9be3aa991870b4c430b5a2ca68a78cf45713e0
JulioASMB <julioasmb@gmail.com>
2022-04-27 23:24:06 -0400

    entrega coordenadas entrada y curvadere arreglado

--- classes/Curva_class.py
+++ classes/Curva_class.py
@@ -1,16 +1,16 @@
 
-import re
 from user_class import User, usuario
-from math import sqrt, atan, tan, sin, degrees, cos, acos, pi
+from math import radians, sqrt, atan, tan, sin, degrees, cos, acos, pi
 
 
 class Curva():
-    def __init__(self, id, project_name, vertices, radio=200, intervalo=10, dm_inicial=0) -> None:
+    def __init__(self, id, project_name, vertices, radio=200, intervalo=10, dm_inicial=0, vp=30) -> None:
         self.__id = id
         self.__project_name = project_name
         self.__vertices = vertices
         self.__intervalo = intervalo
         self.__dm_inicial = dm_inicial
+        self.__vp = vp
         self.__radio = radio
         # ---------variables globales (desempaquetado)---------
         self.__v1, self.__v2, self.__v3 = self.__vertices
@@ -31,6 +31,10 @@ class Curva():
     @property
     def Get_project_name(self):
         return self.__project_name
+
+    @property
+    def Get_vp(self):
+        return self.__vp
 
     @property
     def Get_intervalo(self):
@@ -63,28 +67,35 @@ class Curva():
                     if type(j) == float:
                         self.__vertices = valor
                     else:
-                        return "Esperaba numeros"
+                        return "Se esperaban numeros Reales"
 
     @Get_intervalo.setter
     def Set_intervalo(self, valor):
         if type(valor) == int:
             self.__intervalo = valor
         else:
-            return "intervalo debe ser un entero"
+            return "intervalo debe pertenecer a los enteros"
+
+    @Get_vp.setter
+    def Set_vp(self, valor):
+        if type(valor) == float or type(valor) == int:
+            self.__vp = valor
+        else:
+            return "valor debe pertenecer a los reales"
 
     @Get_dm_inicial.setter
     def Set_dm_inicial(self, valor):
-        if type(valor) == float:
+        if type(valor) == float or type(valor) == int:
             self.__dm_inicial = valor
         else:
-            return "no es un valor numerico"
+            return "valor debe pertenecer a los reales"
 
     @Get_radio.setter
     def Set_radio(self, valor):
-        if type(valor) == float and valor > 0:
+        if (type(valor) == float or type(valor) == int) and valor > 0:
             self.__radio = valor
         else:
-            return "radio debe ser mayor a cero"
+            return "radio debe ser mayor a cero y pertenecer a los reales"
 
 # --------------------elementos geometricos-----------------------
 
@@ -121,7 +132,8 @@ class Curva():
             elif self.__DeltaEste3 < 0 and self.__DeltaNorte3 < 0:
                 azimuthThree = pi + atan(self.__DeltaEste3/self.__DeltaNorte3)
             else:
-                azimuthThree = 2*pi + atan(self.__DeltaEste3/self.__DeltaNorte3)
+                azimuthThree = 2*pi + \
+                    atan(self.__DeltaEste3/self.__DeltaNorte3)
         except ZeroDivisionError:
             return "Ha ocurridio una division por 0"
 
@@ -132,24 +144,32 @@ class Curva():
         return 2*pi-(acos((distancia_1_2**2+distancia_2_3**2-(distancia_1_3**2))/(2*distancia_1_2*distancia_2_3)))
 
     def curvaDerecha(self):  # arreglar
-        if self.anguloExterior()-(self.anguloExterior()- self.azimuts()[0]) > pi:
+        az1, az2, az3 = self.azimuts()
+        del az3
+        if az1 > pi and az2 < pi/2:
+            az2 = 2*pi + az2
+        elif az1 < pi/2 and az2 > pi:
+            az1 = 2*pi + az1
+
+        if az1 < az2:
             return True
-        else:
-            return False
+        return False
+
+    def alpha_medio(self):
+        try:
+            azimutOne, azimutTwo, azimuthThree = self.azimuts()
+            del azimuthThree
+            if azimutOne > azimutTwo:
+                return radians((azimutOne-azimutTwo)/2)
+            return radians((azimutTwo-azimutOne)/2)
+        except ValueError:
+            pass
 
     def Alfa(self):
         if self.curvaDerecha():
             return pi+(self.alpha_medio()*2)
         else:
             return pi-(self.alpha_medio()*2)
-    def alpha_medio(self):
-        try:
-            azimutOne, azimutTwo, azimuthThree = self.azimuts()
-            if azimutOne > azimutTwo:
-                return (azimutOne-azimutTwo)/2
-            return(azimutTwo-azimutOne)/2
-        except ValueError:
-            pass
 
     def Tangente(self):
         try:
@@ -173,7 +193,7 @@ class Curva():
         pass
 
     def Resumen(self):
-        return f"A: {round(degrees(self.Alfa())*(10/9),4)}g\nr: {round(self.__radio,3)}"
+        return f"VP: {self.__vp} km/h\nA: {round(degrees(self.Alfa())*(10/9),4)}g\nR: {round(self.__radio,3)}m\nT: {round(self.Tangente(),3)}m\nS: {round(self.Secante(),3)}m\nDC: {round(self.desarrolloCurva(),3)}m"
 
     # -------------main------------------------
 
@@ -181,24 +201,30 @@ class Curva():
         distancia = self.__dm_inicial + \
             self.distancias_vertices()[1]-self.Tangente()
         dms = []
-        for i in range(int(self.__dm_inicial), int(distancia), self.__intervalo):
-            dms.append(i)
-        dms.pop(0)
-        dms.insert(0, self.__dm_inicial)
-        return dms
+        try:
+            for i in range(int(self.__dm_inicial), int(distancia), self.__intervalo):
+                dms.append(i)
+            dms.pop(0)
+            dms.insert(0, self.__dm_inicial)
+            return dms
+        except TypeError:
+            pass
 
     def DmsCurva(self):
         distancia = self.__dm_inicial + \
             self.distancias_vertices()[1]-self.Tangente()
         dms = []
-        for i in range(self.DmsLineaEntrada()[-1], int(distancia+self.desarrolloCurva()), self.__intervalo):
-            if i > distancia:
-                dms.append(i)
-            else:
-                continue
-        dms.insert(0, distancia)
-        dms.append(distancia+self.desarrolloCurva())
-        return dms
+        try:
+            for i in range(self.DmsLineaEntrada()[-1], int(distancia+self.desarrolloCurva()), self.__intervalo):
+                if i > distancia:
+                    dms.append(i)
+                else:
+                    continue
+            dms.insert(0, distancia)
+            dms.append(distancia+self.desarrolloCurva())
+            return dms
+        except TypeError:
+            pass
 
     def DmsLineaSalida(self):
         distanciaFc = self.__dm_inicial + \
@@ -210,16 +236,34 @@ class Curva():
             self.desarrolloCurva() + \
             self.distancias_vertices()[2]-self.Tangente()
         dms = []
-        for i in range(self.DmsCurva()[-2], int(distancia), self.__intervalo):
-            if i > distanciaFc:
-                dms.append(i)
-        dms.append(distancia)
-        return dms
+        try:
+            for i in range(self.DmsCurva()[-2], int(distancia), self.__intervalo):
+                if i > distanciaFc:
+                    dms.append(i)
+            dms.append(distancia)
+            return dms
+        except TypeError:
+            pass
+
+    def coordenadasEntrada(self):
+        az1, az2, az3 = self.azimuts()
+        del az2, az3
+        dh = []
+        coordenadas = []
+        for i in self.DmsLineaEntrada():
+            dh.append(i-self.__dm_inicial)
+        for i in dh:
+            coordenadas.append(
+                (round(self.__e1+i*sin(az1), 3), round(self.__n1 + i*cos(az1), 3)))
+        return coordenadas
 
 
-curva = Curva(1, "Linares", ((1000, 1000), (1500, 1500),
-                             (2000, 1300)), 500, 20, 100)
+curva = Curva(1, "Linares", ((1000, 1000), (1500, 1450),
+              (500, 1400)), 3000, 10, 50, 30)
 print(curva.DmsLineaEntrada())
 print(curva.DmsCurva())
 print(curva.DmsLineaSalida())
 print(curva.curvaDerecha())
+curva.Set_vp = 40
+print(curva.Resumen())
+print(curva.coordenadasEntrada())
